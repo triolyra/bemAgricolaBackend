5d04b0ef76bba2a9363d4f3dece7fc468ba6c947
Ícaro Garcez <allegarcezz@gmail.com>
2021-12-07 17:01:11 -0300

    Hello world!

---
+++ src/main/java/com/itau/carambola_lyra/bemAgricola/BemAgricolaApplication.java
@@ -0,0 +1,13 @@
+package com.itau.carambola_lyra.bemAgricola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemAgricolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemAgricolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itau/carambola_lyra/bemAgricola/BemAgricolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.carambola_lyra.bemAgricola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemAgricolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
