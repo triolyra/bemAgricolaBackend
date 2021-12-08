daa2bf1da8d1f5120feea62711f544cbcdeec962
jujups <giulllia_mrs@yahoo.com>
2021-12-08 11:36:16 -0300

    Adicionando pacotes

---
+++ src/main/java/com/itau/carambola_lyra/bemAgricola/controller/BemAgricolaController.java
@@ -0,0 +1,10 @@
+package com.itau.carambola_lyra.bemAgricola.controller;
+
+public class BemAgricolaController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
