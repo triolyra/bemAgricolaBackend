d4288e4c744e966bbcb6a20fa2a1b4bf19a14e86
Michele Diones <pereiradionesm@gmail.com>
2021-12-08 20:27:26 -0300

    ainiciando entidade bem

--- src/main/java/com/itau/carambola_lyra/bemAgricola/entity/BemAgricolaEntity.java
+++ src/main/java/com/itau/carambola_lyra/bemAgricola/entity/BemAgricolaEntity.java
@@ -1,10 +1,45 @@
 package com.itau.carambola_lyra.bemAgricola.entity;
 
-public class BemAgricolaEntity {
+import java.util.Date;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-	}
+		@Entity
+		@Table(name = "bemAgricola")
+	
+		public class BemAgricolaEntity {
+
+			@Id
+			@GeneratedValue(strategy = GenerationType.IDENTITY)
+			private int codigoBem;
+
+			private String tipoAgricola;
+
+			private String funcionalUsuario;
+
+			private int safra;
+
+			private String quantidade;
+			
+			private boolean colheita;
+
+			private String unidadeMedida;
+			
+			private String tipoMoeda;
+			
+			private double valor;
+
+			@Temporal(TemporalType.TIMESTAMP)
+			private Date dataInclusao = new java.sql.Date(System.currentTimeMillis());
+
+		
+			private Date dataAvaliacao;
+			
 
 }
