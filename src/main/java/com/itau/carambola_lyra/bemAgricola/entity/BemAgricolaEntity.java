ddc1963e4f25538982df92e896617f78e6ddaf21
Michele Diones <pereiradionesm@gmail.com>
2021-12-08 21:38:24 -0300

    adicionando validation e notnull

--- src/main/java/com/itau/carambola_lyra/bemAgricola/entity/BemAgricolaEntity.java
+++ src/main/java/com/itau/carambola_lyra/bemAgricola/entity/BemAgricolaEntity.java
@@ -9,37 +9,135 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+//import javax.validation.constraints.Size;
 
-		@Entity
-		@Table(name = "bemAgricola")
-	
-		public class BemAgricolaEntity {
+@Entity
+@Table(name = "tb_bemAgricola")
 
-			@Id
-			@GeneratedValue(strategy = GenerationType.IDENTITY)
-			private int codigoBem;
+public class BemAgricolaEntity {
 
-			private String tipoAgricola;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int codigoBem;
 
-			private String funcionalUsuario;
+	@NotNull
+	private String tipoAgricola;
 
-			private int safra;
+	@NotNull
+	private String funcionalUsuario;
 
-			private String quantidade;
-			
-			private boolean colheita;
+	@NotNull
+	private int safra;
 
-			private String unidadeMedida;
-			
-			private String tipoMoeda;
-			
-			private double valor;
+	@NotNull
+	private String quantidade;
 
-			@Temporal(TemporalType.TIMESTAMP)
-			private Date dataInclusao = new java.sql.Date(System.currentTimeMillis());
+	@NotNull
+	private boolean colheita;
 
-		
-			private Date dataAvaliacao;
-			
+	@NotNull
+	private String unidadeMedida;
+
+	@NotNull
+	private String tipoMoeda;
+
+	@NotNull
+	private double valor;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataInclusao = new java.sql.Date(System.currentTimeMillis());
+
+	@NotNull
+	private Date dataAvaliacao;
+
+	public int getCodigoBem() {
+		return codigoBem;
+	}
+
+	public void setCodigoBem(int codigoBem) {
+		this.codigoBem = codigoBem;
+	}
+
+	public String getTipoAgricola() {
+		return tipoAgricola;
+	}
+
+	public void setTipoAgricola(String tipoAgricola) {
+		this.tipoAgricola = tipoAgricola;
+	}
+
+	public String getFuncionalUsuario() {
+		return funcionalUsuario;
+	}
+
+	public void setFuncionalUsuario(String funcionalUsuario) {
+		this.funcionalUsuario = funcionalUsuario;
+	}
+
+	public int getSafra() {
+		return safra;
+	}
+
+	public void setSafra(int safra) {
+		this.safra = safra;
+	}
+
+	public String getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(String quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public boolean isColheita() {
+		return colheita;
+	}
+
+	public void setColheita(boolean colheita) {
+		this.colheita = colheita;
+	}
+
+	public String getUnidadeMedida() {
+		return unidadeMedida;
+	}
+
+	public void setUnidadeMedida(String unidadeMedida) {
+		this.unidadeMedida = unidadeMedida;
+	}
+
+	public String getTipoMoeda() {
+		return tipoMoeda;
+	}
+
+	public void setTipoMoeda(String tipoMoeda) {
+		this.tipoMoeda = tipoMoeda;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public Date getDataInclusao() {
+		return dataInclusao;
+	}
+
+	public void setDataInclusao(Date dataInclusao) {
+		this.dataInclusao = dataInclusao;
+	}
+
+	public Date getDataAvaliacao() {
+		return dataAvaliacao;
+	}
+
+	public void setDataAvaliacao(Date dataAvaliacao) {
+		this.dataAvaliacao = dataAvaliacao;
+	}
 
 }
