c101236de006e306d3531178da2a3a22fc04c94c
Michele Diones <pereiradionesm@gmail.com>
2021-12-13 14:07:55 -0300

    estrutura do inicio da service

--- src/main/java/com/itau/carambola_lyra/bemAgricola/service/BemAgricolaService.java
+++ src/main/java/com/itau/carambola_lyra/bemAgricola/service/BemAgricolaService.java
@@ -1,10 +1,26 @@
 package com.itau.carambola_lyra.bemAgricola.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itau.carambola_lyra.bemAgricola.entity.BemAgricolaEntity;
+import com.itau.carambola_lyra.bemAgricola.repository.BemAgricolaRepository;
+import com.itau.carambola_lyra.bemAgricola.usecase.CadastrarUseCase;
+
+@Service
 public class BemAgricolaService {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@Autowired
+	private BemAgricolaRepository repositoryBemAgricola;
 
+	public Optional<Object> cadastrarCodigo(CadastrarUseCase novoCadastro) {
+		return null;
+
+}
+
+	public void post(BemAgricolaEntity cadastroBem) {
+		
 	}
-
 }
